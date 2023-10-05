be71e8e994b68a27b8969cea8826c6067e08dc55
Jayaprakash Sahoo <aryakumarjayaprakash@gmail.com>
2023-10-06 01:26:59 +0530

    Basic navbar done

--- .history/app/layout_20231006005743.tsx
+++ .history/app/layout_20231006005743.tsx
@@ -1,4 +1,3 @@
-import Navbar from '@/components/Navbar'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter, Space_Grotesk } from 'next/font/google'
---
+++ .history/app/page_20231006012310.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+      <section className=' px-6 border-2 md:px-20 border-red-500 py-24'>
+        
+      </section>
+    </>
+  )
+}
+
+export default Home
